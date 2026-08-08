b210dc0223db57c2d1f93030e2c3f1df7c4b3fb8
aigul <ysakova90@gmail.com>
2026-08-08 20:17:52 +0000

    changed main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "helm_release" "this" {
-  name       = var.name
-  repository = var.repository
-  chart      = var.chart
-  namespace = var.namespace
+module name {
+  source     = "ysakova90/appdeploy/helm" 
+  name       = "nginx-ingress-controller"
+  namespace  = "default"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
 }
 
