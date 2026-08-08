00b9456f1490999f454014f58eece72edf934bac
aigul <ysakova90@gmail.com>
2026-08-08 20:21:34 +0000

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,21 +1,23 @@
 variable "name" {
-  description = "please specify a name for your deployment"
+  description = "Please specify a name for your deployment"
   type        = string
   default     = "demo"
 }
+
 variable "namespace" {
-  description = "please specify a namespace for your deployment"
+  description = "Please specify a namespace for your deployment"
   type        = string
-  default = "default"
+  default     = "default"
 }
+
 variable "repository" {
-  description = "please specify a repository for your deployment"
+  description = "Please specify a repository for your deployment"
   type        = string
   default     = null
 }
 
 variable "chart" {
-  description = "please specify a chart for your deployment"
+  description = "Please specify a chart for your deployment"
   type        = string
 }
 
